469da635fc87d223a5c6ab98bc0c92c722ba26ef
Dolores <dee12098@yandex.ru>
2022-12-18 17:12:57 +0300

    added dz

---
+++ dz41/Program.cs
@@ -0,0 +1,27 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь. Ввод чисел останавливается 
+// при помощи ввода слова "stop"
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+
+int count = 0;
+
+Console.WriteLine("Введите число: ");
+
+string? num = Console.ReadLine();;
+
+while (num != "stop") 
+{
+    int num1 = Convert.ToInt32(num);
+    if (num1 > 0) count++;
+    num = Console.ReadLine();
+}
+
+Console.WriteLine(count);
+
+
+
+
+
+
---
+++ dz43/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+double getNum(string message)
+{
+    Console.WriteLine(message);
+    double num = 0;
+    return num = Convert.ToDouble(Console.ReadLine());
+}
+
+double k1 = getNum("Введите значение k1: ");
+double b1 = getNum("Введите значение b1: ");
+double k2 = getNum("Введите значение k2: ");
+double b2 = getNum("Введите значение b2: ");
+
+if (k1 != k2) 
+{
+    double x = (b1 + (-b2)) / (k2 + (-k1));
+    double y = k1 * x + b1;
+    Console.WriteLine($"Координаты точки пересечения двух прямых: {x}, {y}");
+}
+else Console.WriteLine("Прямые параллельны, точки пересечения нет.");
+
+
